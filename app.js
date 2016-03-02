416efe13597587cb1346b4871707776c5f68b223
Hank Andre <hankandre@gmail.com>
2016-03-02 07:19:28 -0600

    updated css and appearance

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// global fruits array which holds objects of fruits
 var fruits=[];
 
 function Fruit (name, price, averagePrice, inventory, className){
@@ -13,9 +14,7 @@ var randomInitialPrice = function() {
   return randomNumber(50, 999);
 
 }
-
-console.log(randomInitialPrice());
-
+//global variables
 var apple = new Fruit("Apples", randomInitialPrice(), [], 0, "apple");
 var banana = new Fruit("Bananas", randomInitialPrice(), [], 0, "banana");
 var orange = new Fruit("Oranges", randomInitialPrice(), [], 0, "orange");
@@ -27,23 +26,70 @@ var counter = 0;
 var timer;
 var cash=10000;
 
+//funciton begin first game
 function beginGame() {
-  timer = setInterval(priceUpdate, 3000);
-  console.log("begin");
+  timer = setInterval(priceUpdate, 15000);
   priceUpdate();
-
 }
 
+//function try again
+function tryAgain () {
+  $(".blur").removeClass("blur");
+  $(".message").hide();
+  for(i=0; i<fruits.length;i++){
+    fruits[i].averagePrice=[];
+    $('.'+fruits[i].className+'-expense').html(0);
+  }
+  cash=10000;
+  $('.bank').html('100.00')
+  beginGame();
+}
 
-
+// get random number function
 function randomNumber(min, max){
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+//main funciton priceUpdate updates every 5 sec. and will be stoped at 5 min.
+function priceUpdate() {
+  counter++;
+  if (counter == 20) { //change coutner latter to 20
+
+    for(i=0; i<fruits.length;i++){
+      if(fruits[i].inventory>0){
+        var fireSale=0;
+        fireSale=fruits[i].inventory*fruits[i].price;
+        fruits[i].inventory=0;
+        $('.'+fruits[i].className+'-inventory').html(fruits[i].inventory);
+        cash+=fireSale;
+      }
+    }
+    $('.bank').html(cash/100);
+    clearInterval(timer);
+    $(".main").addClass("blur");
+    $('.alert-message').html("You earned: $"+(cash-10000)/100)
+    $('.message').show();
+    //alert("Your you earned: $"+(cash-10000)/100);
+  }
+
+  for(var i=0;i<fruits.length; i++){
+    var priceUpdate=0;
+    var priceUpdate = randomNumber(-50, 50);
+    fruits[i].price = fruits[i].price + priceUpdate;
+    if(fruits[i].price >= 1000){
+      fruits[i].price -= 49;
+    }
+    else if (fruits[i].price < 50) {
+      fruits[i].price += 49;
+    }
+    $('.'+fruits[i].className+'-price').text(fruits[i].price / 100);
+  }
+
+}
+
+//buy funciton
 function buy(){
-  console.log(this);
    var myClass = $(this).data('classname');
-   console.log(myClass);
    for( i=0; i<fruits.length; i++){
      if(myClass==fruits[i].className && cash>=fruits[i].price){
 
@@ -59,71 +105,33 @@ function buy(){
    }
 }
 
+// fuction to calculate average purchase price per fruit
 function calcAverage(array){
   var sum=0;
   for(var j=0; j<array.length; j++){
     sum+=parseInt(array[j]);
-
   }
   return sum/array.length;
 }
 
+//sell funciton
 function sell() {
   console.log(this);
   var myClass = $(this).data('classname');
   console.log(myClass);
   for( i=0; i<fruits.length; i++){
     if(myClass==fruits[i].className && fruits[i].inventory > 0){
-
       cash+=fruits[i].price;
       fruits[i].inventory--;
       $('.'+fruits[i].className+'-inventory').html(fruits[i].inventory);
       $('.bank').html(cash/100);
      }
   }
-
 }
 
-function priceUpdate() {
-  counter++;
-
-
-  if (counter == 10) { //change coutner latter
-
-    for(i=0; i<fruits.length;i++){
-      if(fruits[i].inventory>0){
-        var fireSale=0;
-        fireSale=fruits[i].inventory*fruits[i].price;
-        fruits[i].inventory=0;
-        $('.'+fruits[i].className+'-inventory').html(fruits[i].inventory);
-        cash+=fireSale;
-
-
-      }
-    }
-    $('.bank').html(cash/100);
-    alert("Your you earned: $"+(cash-10000)/100);
-    clearInterval(timer);
-  }
-
-
-  for(var i=0;i<fruits.length; i++){
-    var priceUpdate=0;
-    var priceUpdate = randomNumber(-50, 50);
-    // priceUpdate = priceUpdate;
-    fruits[i].price = fruits[i].price + priceUpdate;
-    if(fruits[i].price >= 1000){
-      fruits[i].price -= 49;
-    }
-    else if (fruits[i].price < 50) {
-      fruits[i].price += 49;
-    }
-    $('.'+fruits[i].className+'-price').text(fruits[i].price / 100);
-  }
-
-}
 
 $(document).ready(function() {
+
   $('.start').on('click', beginGame);
 
   $('.apple-buy').on('click', buy);
@@ -136,5 +144,6 @@ $(document).ready(function() {
   $('.banana-sell').on('click', sell);
   $('.pear-sell').on('click', sell);
 
+  $('.start-over-button').on('click', tryAgain);
 
 });
